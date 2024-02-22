e8281a5f61fda4ab92c0743c9f3721ba3426fc4a
unknown <loicHounang>
2024-02-22 09:29:30 +0100

    adding about page

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ApplicationHomePage from "./views/HomePage";
+import About from "./views/About";
 
 const ApplicationRoutes = () => {
     
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/univsoft-website" element={ <ApplicationHomePage /> } />
+                <Route path="/univsoft-website/" element={ <ApplicationHomePage /> } />
+                <Route path="/univsoft-website/about/" element={ <About /> } />
             </Routes>
         </BrowserRouter>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
           <a class="nav-link active "  href="#">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">A propos</a>
+          <a class="nav-link" href="/univsoft-website/about/">A propos</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
